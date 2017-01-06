2c16c98e5bf8a38156800846d4f2df246626177c
Tom Smalley <tfsmalley@gmail.com>
2017-01-06 19:55:37 +0000

    Setup stack

---
+++ README.md
@@ -0,0 +1,3 @@
+Work in progress:
+
+Chapter 2: Agents
---
+++ src/AIMA.hs
@@ -0,0 +1,7 @@
+module AIMA
+    ( module AIMA.Agent.Core
+    , module AIMA.Agent.Vacuum
+    ) where
+
+import AIMA.Agent.Core
+import AIMA.Agent.Vacuum
